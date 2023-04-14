bd404e21d201e4668e651d41fe4a3380a99562ab
Matthew Streets <87211678+matthewstreets@users.noreply.github.com>
2023-04-14 21:43:51 +0100

    finished part 1a

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,13 +7,22 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  console.log(props)
+const Part = (props) => {
   return (
     <div>
       <p>
         {props.name} {props.exercises}
       </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.name} exercises={props.exercises}/>
+      <Part name={props.name2} exercises={props.exercises2} />
+      <Part name={props.name3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -41,9 +50,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content name={part1} exercises={exercises1} /> 
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
+      <Content name={part1} exercises={exercises1} name2={part2} exercises2={exercises2} name3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
